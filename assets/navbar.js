b721165005f092d1fc9ff534868b6e9d4f23ba88
Parashara <parashara@gmail.com>
2019-04-04 12:56:59 -0700

    updated lots of visuals and wireframes

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,22 +1,21 @@
-$(document).ready(function() {
-  $(".menu-icon").on("click", function() {
-        $("nav ul").toggleClass("showing");
-  });
+$(document).ready(function () {
+      $(".menu-icon").on("click", function () {
+            $("nav ul").toggleClass("showing");
+      });
 });
 
 // Scrolling Effect
+$(window).on("scroll", function () {
+      if (window.scrollY !== 0) {
+            $('nav').addClass('black');
+            $('.logo').css('color', 'white')
+            $('a').css('color', 'white')
+      }
 
-$(window).on("scroll", function() {
-  if($(window).scrollTop()) {
-        $('nav').addClass('black');
-        $('.logo').css('color', 'white')
-        $('a').css('color', 'white')
-  }
-
-  else {
-        $('nav').removeClass('black');
-        $('.logo').css('color', '#000058')
-        $('a').css('color', '#000058')
-  }
+      else if ($(window).width() > 1040) {
+            $('nav').removeClass('black');
+            $('.logo').css('color', '#000058')
+            $('a').css('color', '#000058')
+      }
 })
 
